245a1377833a39f14eea16399d11f33b91ff2d9b
karol marques <karoldm1579@gmail.com>
2023-01-03 16:02:37 -0300

    refactor convert AF -> ER

--- automato/getExprReg.js
+++ automato/getExprReg.js
@@ -32,6 +32,66 @@ export default function getExprReg(currState, expressions, nextStates, exprStr, 
         expressions.push(exprStr);
     }
 
+    //tratamento de loop 
+    //verificar se existe algum nextStates que esta em linksVisited
+    //se sim, fazer o tratamento (contar parenteses e inserir fechamento)
+    //após isso, se o nextState verificado por final (incluir em expressions e remover esse caminho)
+    //se ele não for final, pular para ele e tentar achar um estado final
+    //seguir busca no currState para achar um estado final
+    let loop = false;
+    let statesLoops = [];
+    nextStates.forEach((next) => {
+        if (linksVisited.includes(next.to)) {
+            statesLoops.push(next)
+            loop = true;
+        }
+    });
+
+    while (loop) {
+        let nextState = statesLoops[0];
+
+        let back = Math.abs((linksVisited.length - 1) - linksVisited.indexOf(nextState.to));
+
+        let exprCopy = exprStr;
+        //voltar back quantidades de pares de parenteses em exprStr
+        let aux = "";
+        let exprSepareted = [];
+
+        for (let i = 0; i < exprCopy.length; i++) {
+            aux += exprCopy[i];
+            if (aux.includes(")")) {
+                if (exprCopy[i + 1] === "*") {
+                    aux += exprCopy[i + 1];
+                    i++;
+                }
+                exprSepareted.push(aux);
+                aux = "";
+            }
+        }
+        exprSepareted[exprSepareted.length - back] = `(${exprSepareted[exprSepareted.length - back]}`;
+        exprSepareted.push(`(${nextState.text == "" ? "ɛ" : nextState.text}))*`);
+
+        //se o estado que leva ao loop é final, inserimos a expressão obtida
+        if (finalNodes.includes(nextState.to)) {
+            exprStr = exprSepareted.join("");
+            expressions.push(exprStr);
+        }
+        //se o estado atual é final, atualizamos a expressão atual
+        else if (finalNodes.includes(currState)) {
+            expressions.push(exprStr + exprSepareted.join(""));
+        }
+        //se nenhum dos dois é final
+        else {
+            exprStr = exprSepareted.join("");
+            expressions.push(exprStr);
+        }
+        //eliminando esse estado
+        statesLoops = statesLoops.filter((link) => link.to !== nextState.to);
+        if (statesLoops.length === 0) {
+            loop = false;
+        }
+    }
+
     while (nextStates.length > 0) {
         let nextState = nextStates[0];
 
